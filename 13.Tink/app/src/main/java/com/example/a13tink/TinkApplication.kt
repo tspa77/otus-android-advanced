d6b67589105e17eb41116f93d9aad81b4ca1af3f
Pavel (Home) <tspa77@gmail.com>
2019-12-03 00:06:21 +0700

    wip

--- 13.Tink/app/src/main/java/com/example/a13tink/TinkApplication.kt
+++ 13.Tink/app/src/main/java/com/example/a13tink/TinkApplication.kt
@@ -1,35 +1,37 @@
 package com.example.a13tink
 
-import android.app.Application
-import com.google.crypto.tink.Aead
-import com.google.crypto.tink.KeysetHandle
-import com.google.crypto.tink.aead.AeadConfig
-import com.google.crypto.tink.aead.AeadKeyTemplates
-import com.google.crypto.tink.integration.android.AndroidKeysetManager
-
-/** A custom application that initializes the Tink runtime at application startup.  */
-class TinkApplication : Application() {
-    companion object {
-        private const val PREF_FILE_NAME = "master_pref"
-        private const val TINK_KEYSET_NAME = "master_keyset"
-        private const val MASTER_KEY_URI = "android-keystore://master_key"
-    }
-
-    lateinit var aead: Aead
-
-    override fun onCreate() {
-        super.onCreate()
-
-        AeadConfig.register()
-        aead = getOrGenerateNewKeysetHandle().getPrimitive(Aead::class.java)
-    }
-
-
-    private fun getOrGenerateNewKeysetHandle(): KeysetHandle =
-        AndroidKeysetManager.Builder()
-            .withKeyTemplate(AeadKeyTemplates.AES256_GCM)
-            .withMasterKeyUri(MASTER_KEY_URI)
-            .withSharedPref(applicationContext, TINK_KEYSET_NAME, PREF_FILE_NAME)
-            .build()
-            .keysetHandle
-}
+//package com.example.a13tink
+//
+//import android.app.Application
+//import com.google.crypto.tink.Aead
+//import com.google.crypto.tink.KeysetHandle
+//import com.google.crypto.tink.aead.AeadConfig
+//import com.google.crypto.tink.aead.AeadKeyTemplates
+//import com.google.crypto.tink.integration.android.AndroidKeysetManager
+//
+///** A custom application that initializes the Tink runtime at application startup.  */
+//class TinkApplication : Application() {
+//    companion object {
+//        private const val PREF_FILE_NAME = "master_pref"
+//        private const val TINK_KEYSET_NAME = "master_keyset"
+//        private const val MASTER_KEY_URI = "android-keystore://master_key"
+//    }
+//
+//    lateinit var aead: Aead
+//
+//    override fun onCreate() {
+//        super.onCreate()
+//
+//        AeadConfig.register()
+//        aead = getOrGenerateNewKeysetHandle().getPrimitive(Aead::class.java)
+//    }
+//
+//
+//    private fun getOrGenerateNewKeysetHandle(): KeysetHandle =
+//        AndroidKeysetManager.Builder()
+//            .withKeyTemplate(AeadKeyTemplates.AES256_GCM)
+//            .withMasterKeyUri(MASTER_KEY_URI)
+//            .withSharedPref(applicationContext, TINK_KEYSET_NAME, PREF_FILE_NAME)
+//            .build()
+//            .keysetHandle
+//}
