d233c81e868063442ab78daaa2223bfd7820e0ce
Pavel (Home) <tspa77@gmail.com>
2019-12-08 16:18:17 +0700

    закончил кодирование/декодирование строки

--- 13.SecurityTink/app/src/main/java/com/example/securitytink/MainActivity.kt
+++ 13.SecurityTink/app/src/main/java/com/example/securitytink/MainActivity.kt
@@ -7,21 +7,25 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var coder: Coder
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        coder = Coder(applicationContext)
+
         et_plain_text.doAfterTextChanged { codingString(it.toString()) }
-
-
     }
 
 
     private fun codingString(text: String?) {
-
-
-        tv_encrypted.text = text
-
+        if (!text.isNullOrEmpty()) {
+            tv_encrypted.text = coder.attemptEncrypt(text)
+            tv_decrypted.text = coder.attemptDecrypt(tv_encrypted.text.toString())
+        } else {
+            tv_encrypted.text = ""
+            tv_decrypted.text = ""
+        }
     }
 }
-
