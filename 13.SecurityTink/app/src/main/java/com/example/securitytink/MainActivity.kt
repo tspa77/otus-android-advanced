ec19986e1056f7104d53f630506d913e341870be
Pavel (Home) <tspa77@gmail.com>
2019-12-09 00:17:51 +0700

    Красоту навёл и сделал сохранение в Shared Preferences

--- 13.SecurityTink/app/src/main/java/com/example/securitytink/MainActivity.kt
+++ 13.SecurityTink/app/src/main/java/com/example/securitytink/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.securitytink
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doAfterTextChanged
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,15 +18,24 @@ class MainActivity : AppCompatActivity() {
         tinkApp = applicationContext as TinkApplication
 
         et_plain_text.doAfterTextChanged { codingString(it.toString()) }
+        btn_save_sp.setOnClickListener { saveSP(et_plain_text.text.toString()) }
+        btn_load_sp.setOnClickListener { loadSP() }
+    }
+
+    private fun saveSP(text: String?) {
+        tinkApp.saveSP(text ?: "")
+        Toast.makeText(this, "String saved to Shared Preferences", Toast.LENGTH_SHORT)
+            .show()
+    }
+
+    private fun loadSP() {
+        tv_sp_key.text = tinkApp.loadSP()
+        Toast.makeText(this, "String loaded from Shared Preferences", Toast.LENGTH_SHORT)
+            .show()
     }
 
     private fun codingString(text: String?) {
-        if (!text.isNullOrEmpty()) {
-            tv_encrypted.text = tinkApp.attemptEncrypt(text)
-            tv_decrypted.text = tinkApp.attemptDecrypt(tv_encrypted.text.toString())
-        } else {
-            tv_encrypted.text = ""
-            tv_decrypted.text = ""
-        }
+        tv_encrypted.text = tinkApp.encryptString(text ?: "")
+        tv_decrypted.text = tinkApp.decryptString(tv_encrypted.text.toString())
     }
 }
--- 13.SecurityTink/app/src/main/java/com/example/securitytink/TinkApplication.kt
+++ 13.SecurityTink/app/src/main/java/com/example/securitytink/TinkApplication.kt
@@ -1,25 +1,42 @@
 package com.example.securitytink
 
 import android.app.Application
+import android.content.SharedPreferences
 import android.util.Base64
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
 import com.google.crypto.tink.Aead
 import com.google.crypto.tink.aead.AeadKeyTemplates
 import com.google.crypto.tink.config.TinkConfig
 import com.google.crypto.tink.integration.android.AndroidKeysetManager
 import java.nio.charset.StandardCharsets
 
+
 class TinkApplication : Application() {
 
     private val MASTER_KEY_URI = "android-keystore://master_key"
     private val PREF_FILE_NAME = "master_pref"
+    private val PREF_KEY = "key"
     private val TINK_KEYSET_NAME = "master_keyset"
     private val EMPTY_ASSOCIATED_DATA = ByteArray(0)
-    lateinit var aead: Aead
+    private lateinit var mySharedPreferences: SharedPreferences
+    private lateinit var aead: Aead
+
 
     override fun onCreate() {
         super.onCreate()
+
         TinkConfig.register()
         aead = getOrGenerateNewKeysetHandle().getPrimitive(Aead::class.java)
+
+        val myMasterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+        mySharedPreferences = EncryptedSharedPreferences.create(
+            PREF_FILE_NAME,
+            myMasterKeyAlias,
+            applicationContext,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV, //for encrypting Keys
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM ////for encrypting Values
+        )
     }
 
     private fun getOrGenerateNewKeysetHandle() = AndroidKeysetManager.Builder()
@@ -28,26 +45,34 @@ class TinkApplication : Application() {
             TINK_KEYSET_NAME,
             PREF_FILE_NAME
         )
-        .withKeyTemplate(AeadKeyTemplates.AES256_GCM)
+        .withKeyTemplate(AeadKeyTemplates.AES128_GCM)
         .withMasterKeyUri(MASTER_KEY_URI)
         .build()
         .keysetHandle
 
 
-    fun attemptEncrypt(text: String): String {
+    fun encryptString(text: String): String {
         val plainText = text.toByteArray(StandardCharsets.UTF_8)
         val cipherText = aead.encrypt(plainText, EMPTY_ASSOCIATED_DATA)
         return base64Encode(cipherText!!)
     }
 
-    fun attemptDecrypt(text: String): String {
+    fun decryptString(text: String): String {
         val cipherText = base64Decode(text)
         val plainText = aead.decrypt(cipherText, EMPTY_ASSOCIATED_DATA)
         return String(plainText, StandardCharsets.UTF_8)
     }
 
+    fun saveSP(saveText: String) {
+        mySharedPreferences.edit()
+            .putString(PREF_KEY, saveText)
+            .apply()
+    }
+
+    fun loadSP(): String {
+        return mySharedPreferences.getString(PREF_KEY, "")!!
+    }
+
     private fun base64Encode(input: ByteArray) = Base64.encodeToString(input, Base64.DEFAULT)
     private fun base64Decode(input: String) = Base64.decode(input, Base64.DEFAULT)
-
-
 }
