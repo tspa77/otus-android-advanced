1d38d791fc7198cbb573e913a6a559259d43e86d
TsygankovPA <tspa77@gmail.com>
2019-12-16 14:01:51 +0700

    mini fix

--- 17.MVP/app/src/main/java/com/example/mvppattern/mvp/view/DetailsViewActivity.kt
+++ 17.MVP/app/src/main/java/com/example/mvppattern/mvp/view/DetailsViewActivity.kt
@@ -3,7 +3,7 @@ package com.example.mvppattern.mvp.view
 import android.os.Bundle
 import android.view.View
 import android.widget.ProgressBar
-import com.example.mvppattern.AppConstants
+import com.example.mvppattern.AppConstants.IMAGE_URL
 import com.example.mvppattern.AppConstants.MOVIE_ID
 import com.example.mvppattern.R
 import com.example.mvppattern.mvp.model.MovieDetails
@@ -39,7 +39,7 @@ class DetailsViewActivity : LoadingViewActivity(), DetailsView {
         tv_overview.text = movieInfo.overview
         cv_vote.setVote(movieInfo.voteAverage.toFloat())
         Picasso.get()
-            .load(AppConstants.IMAGE_URL + movieInfo.posterPath)
+            .load(IMAGE_URL + movieInfo.posterPath)
             .into(iv_poster)
         group_labels.visibility = View.VISIBLE
     }
