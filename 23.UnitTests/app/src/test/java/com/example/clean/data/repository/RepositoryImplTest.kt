c1aa3ab8a43ce928cebf881b6e67dd5fddeea98d
tspa77 <tspa77@gmail.com>
2020-02-18 15:42:25 +0700

    Добавил заготовку под RepositoryImplTest и прописал зависимости в грэдл

---
+++ 23.UnitTests/app/src/test/java/com/example/clean/data/repository/RepositoryImplTest.kt
@@ -0,0 +1,26 @@
+package com.example.clean.data.repository
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class RepositoryImplTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun getListMoviePreviews() {
+    }
+
+    @Test
+    fun getMovieInfo() {
+    }
+}
